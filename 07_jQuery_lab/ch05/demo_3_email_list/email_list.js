1836badf4710593dc1e112bb97b24af1da2171ff
Milena Tomova <kokolina1888@gmail.com>
2017-06-14 18:04:34 +0300

    sync

--- 07_jQuery_lab/ch05/demo_3_email_list/email_list.js
+++ 07_jQuery_lab/ch05/demo_3_email_list/email_list.js
@@ -6,33 +6,13 @@ $(document).ready(function() {
 			var isValid = true;
 			
 			// validate the first email address
-			if (emailAddress1 == "") { 
+			if (emailAddress1 === "") { 
 				$("#email_address1_error").text("This field is required.");
 				isValid = false;
 			} else {
 				$("#email_address1_error").text("");
-			} 
-			
-			// validate the second email address
-			if (emailAddress2 == "") { 
-				$("#email_address2_error").text("This field is required.");
-				isValid = false; 
-			} else if (emailAddress1 !== emailAddress2) { 
-				$("#email_address2_error").text("This entry must equal first entry.");
-				isValid = false;
-			} else {
-				$("#email_address2_error").text("");
-			}
-			
-			// validate the first name entry  
-			if ($("#first_name").val() == "") {
-				$("#first_name_error").text("This field is required.");
-				isValid = false;
-			} 
-			else {
-				$("#first_name_error").text("");
-			}
-			
+			} 			
+					
 			// submit the form if all entries are valid
 			if (isValid) {
 				$("#email_form").submit(); 
--- 07_jQuery_lab/ch05/demo_4_image_swap/image_swap.js
+++ 07_jQuery_lab/ch05/demo_4_image_swap/image_swap.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-	// preload images
-	$("#image_list a").each(function() {
-		var swappedImage = new Image();
-		swappedImage.src = $(this).attr("href");
-	});
 	
 	// set up event handlers for links    
 	$("#image_list a").click(function(evt) {
